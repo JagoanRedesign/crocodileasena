1ad970e7c37c1be2098e796a8b53ce6ab49ec4bf
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2022-06-11 21:19:44 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	token: "",
-	mongoURI: ""
+	token: "5445366046:AAFy80kMBBmJByuIq6dYUlH5l-eAaUpIrnM",
+	mongoURI: "mongodb+srv://emin:emin@cluster0.qr7bw.mongodb.net/?retryWrites=true&w=majority"
 }
