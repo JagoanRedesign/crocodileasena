2c49677eecb64b7a494bda467791d5587bf3dd7e
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2022-06-11 21:57:27 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose')
 
 const {TelegramToken, mongoURI} = require('./config')
