d7ed08e45f4004bcbf7fea807825675127d33cf3
Deniz <asifatonline@gmail.com>
2022-09-18 14:16:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	TelegramToken: "5669948417:AAGxKzHL7eUYHvp8X2muMKysXnKtQRsibkg",
+	TelegramToken: "5669948417:AAGi7uYVNvA8NzEJaDmoFVDLYhSjt_B4elk",
 	mongoURI: "mongodb+srv://asena:341644@cluster0.advkm.mongodb.net/?retryWrites=true&w=majority"
 }
