2e4e9514cd8561f2dff95ddf4ea51f9fb694cf29
Deniz <asifatonline@gmail.com>
2022-09-18 14:32:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'TelegramToken';
-cost token = 'mongoURI';
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
+module.exports = {
+	TelegramToken: "5669948417:AAGxKzHL7eUYHvp8X2muMKysXnKtQRsibkg",
+	TelegramToken: "5669948417:AAGi7uYVNvA8NzEJaDmoFVDLYhSjt_B4elk",
+	mongoURI: "mongodb+srv://asena:341644@cluster0.advkm.mongodb.net/?retryWrites=true&w=majority"
+}
