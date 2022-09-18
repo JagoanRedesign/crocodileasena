4e3f1d991dbbd2db22c4be668939b7f7f2d3b62e
Deniz <asifatonline@gmail.com>
2022-09-18 14:33:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 module.exports = {
-	TelegramToken: "5669948417:AAGxKzHL7eUYHvp8X2muMKysXnKtQRsibkg",
 	TelegramToken: "5669948417:AAGi7uYVNvA8NzEJaDmoFVDLYhSjt_B4elk",
 	mongoURI: "mongodb+srv://asena:341644@cluster0.advkm.mongodb.net/?retryWrites=true&w=majority"
 }
